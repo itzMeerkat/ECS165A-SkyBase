12e22f484e39bb3762cc11e4b13791c06d4c3daa
AG3 <bbfag3@gmail.com>
2020-01-26 18:25:28 -0800

    page ok, table need indirection storage

--- src/page.py
+++ src/page.py
@@ -5,6 +5,7 @@ class Page:
     def __init__(self):
         self.num_records = 0
         self.data = bytearray(PAGE_SIZE)
+        self.free_index = [i for i in range(511, 0, -1)]
         self.MAX_RECORDS = PAGE_SIZE / COL_SIZE
 
     def has_capacity(self):
@@ -17,13 +18,24 @@ class Page:
     TODO: benchmark of byte conversion
     '''
     def write(self, value):
-        l = self.num_records * COL_SIZE
+        insert_index = self.free_index.pop()
+        l = insert_index * COL_SIZE
         r = l + COL_SIZE
         self.data[l:r] = value.to_bytes(8, 'big')
         self.num_records += 1
+        return insert_index
 
+    """
+    TODO: Do we need to check if the offset valid?
+    """
     def read(self, offset):
         l = offset * COL_SIZE
         r = l + COL_SIZE
         value = int.from_bytes(self.data[l:r], 'big')
         return value
+
+
+    def remove(self, index):
+        self.free_index.append(index)
+        self.num_records -= 1
+        return
--- src/table.py
+++ src/table.py
@@ -1,4 +1,4 @@
-from .page import *
+from src.page import *
 from time import time
 
 INDIRECTION_COLUMN = 0
@@ -8,11 +8,62 @@ SCHEMA_ENCODING_COLUMN = 3
 
 
 class Record:
-
     def __init__(self, rid, key, columns):
         self.rid = rid
         self.key = key
         self.columns = columns
+
+
+class Column:
+    def __init__(self):
+        self.base_pages = []
+        self.tail_pages = []
+
+        self.num_base_pages = -1
+        self.num_tail_pages = -1
+
+        # {rid: (tailPageId,offset)}
+        self.latest_lookup = {}
+        # {rid: (basePageId,offset)}
+        self.base_lookup = {}
+
+        self._append_base_page()
+        self._append_tail_page()
+
+
+    def _append_base_page(self):
+        self.base_pages.append(Page())
+        self.num_base_pages += 1
+
+    def _append_tail_page(self):
+        self.tail_pages.append(Page())
+        self.num_tail_pages += 1
+
+    def get_latest_tail(self):
+        latest_tail = self.tail_pages[self.num_tail_pages]
+        if not latest_tail.has_capacity():
+            self._append_tail_page()
+            latest_tail = self.tail_pages[self.num_tail_pages]
+        return latest_tail
+
+    def get_latest_base(self):
+        latest_base = self.base_pages[self.num_base_pages]
+        if not latest_base.has_capacity():
+            self._append_base_page()
+            latest_base = self.base_pages[self.num_base_pages]
+        return latest_base
+
+    def lookup_latest(self, rid):
+        if rid in self.latest_lookup:
+            return 
+
+    def write(self, val):
+        latest = self.get_latest_tail()
+        offset = latest.write(val)
+        return offset
+
+    def read(self, rid):
+
 
 class Table:
 
